b52cb540de6296c9138b0149b7c8409144a28001
Horia Tudosie <htudosie@esri.ca>
2015-07-14 14:34:51 -0400

    mess3

---
+++ ColorContrast/ColorPicker.js
@@ -0,0 +1,268 @@
+var ColorPicker = function() {
+
+    var _private = {
+        colorPickerToolbar: null,
+        colorDiv: null,
+        colorTxt: null,
+        imageUrl: null,
+        showToolbar: false,
+
+        retrieveGlass: function() {
+            var dfr1 = $.Deferred();
+            chrome.storage.sync.get(['magnifierGlass'], function(a) {
+                if (a['magnifierGlass'] != 'none') {
+                    _private.imageUrl = chrome.extension.getURL("Images/" + a['magnifierGlass'] + ".png");
+                    dfr1.resolve();
+                } else {
+                    dfr1.reject();
+                }
+            });
+            return dfr1.promise();
+        },
+
+        retrieveToolbar: function() {
+            var dfr2 = $.Deferred();
+            chrome.storage.sync.get(['toolbar'], function(a) {
+                if (a['toolbar']) {
+                    dfr2.resolve();
+                } else {
+                    dfr2.reject();
+                }
+            });
+            return dfr2.promise();
+        },
+
+        getColor: function(event, type) {
+            var eventTarget = event.target;
+
+            // If the event target is set
+            if (eventTarget) {
+                chrome.extension.sendMessage({
+                    type: "get-color",
+                    x: event.clientX,
+                    y: event.clientY,
+                    eventType: type
+                });
+
+                var ownerDocument = eventTarget.ownerDocument;
+                if (ownerDocument) {
+                    var colorPicker = ownerDocument.getElementById("colorPickerViewer");
+                    if (!colorPicker) return;
+
+                    var tagName = eventTarget.tagName;
+
+                    // If the event target is not the color picker, the color picker is not an ancestor of the event target and the event target is not a scrollbar
+                    if (eventTarget != colorPicker && !_private.isAncestor(eventTarget, colorPicker) && tagName && tagName.toLowerCase() != "scrollbar") {
+                        var w = window.innerWidth - 100;
+                        var h = window.innerHeight - 100;
+                        if (event.clientX < w) {
+                            colorPicker.style.left = event.clientX + 4 + "px";
+                        } else {
+                            colorPicker.style.left = event.clientX - 62 + "px";
+                        }
+                        if (event.clientY < h) {
+                            colorPicker.style.top = event.clientY + 4 + "px";
+                        } else {
+                            colorPicker.style.top = event.clientY - 62 + "px";
+                        }
+                        //console.log(event);
+                    }
+                }
+            }
+        },
+
+        isAncestor: function(element, ancestorElement) {
+            // If the element and ancestor element are set
+            if (element && ancestorElement) {
+                var parentElement = null;
+
+                // Loop through the parent elements
+                while ((parentElement = element.parentNode) !== null) {
+                    // If the parent element is the ancestor element
+                    if (parentElement == ancestorElement) {
+                        return true;
+                    } else {
+                        element = parentElement;
+                    }
+                }
+            }
+
+            return false;
+        },
+
+        Click: function(event) {
+            if (event.button != 2) {
+                _private.getColor(event, "selected");
+
+                event.stopPropagation();
+                event.preventDefault();
+            }
+        },
+
+        MouseMove: function(event) {
+            _private.getColor(event, "hover");
+        },
+
+        removeTitles: function() {
+            //return;
+            $('[title]').bind('mousemove.hideTooltips', function() {
+                $this = $(this);
+                if ($this.attr('title') && $this.attr('title') != '') {
+                    $this.data('title', $this.attr('title'));
+                    $this.attr('title', '');
+                }
+            }).bind('mouseout.hideTooltips', function() {
+                $this = $(this);
+                $this.attr('title', $this.data('title'));
+            });
+        },
+
+        restoreTitles: function() {
+            //return;
+            $('[title]').unbind('mousemove.hideTooltips').unbind('mouseout.hideTooltips');
+            $('*[data-title!=undefined]').attr('title', $(this).data('title'));
+        },
+
+        init: function(contentDocument) {
+            if (!contentDocument.getElementById("colorPickerCursor")) {
+                var colorPickerCss = '<link id="colorPickerCss" rel="stylesheet" type="text/css" href="' + chrome.extension.getURL('ColorPicker.css') + '" />';
+                var css = '<Style id="colorPickerCursor">\n' +
+                    ' * { cursor: url(' + chrome.extension.getURL("Images/Cursors/pickColor.cur") + '), crosshair !important; }\n' +
+                    '</Style>';
+                if ($("head").length == 0) {
+                    $("body").before(colorPickerCss);
+                    $("body").before(css);
+                } else {
+                    $("head").append(colorPickerCss);
+                    $("head").append(css);
+                }
+            }
+
+            contentDocument.addEventListener("click", _private.Click, true);
+            contentDocument.addEventListener("mousemove", _private.MouseMove, false);
+
+            _private.removeTitles();
+
+            _private.retrieveGlass().then(function() {
+                if (!contentDocument.getElementById("colorPickerViewer")) {
+                    ColorPicker.colorPickerViewer = contentDocument.createElement("Div");
+                    ColorPicker.colorPickerViewer.setAttribute("id", "colorPickerViewer");
+
+                    var t = contentDocument.createElement("Table");
+                    ColorPicker.colorPickerViewer.appendChild(t);
+
+                    var d = contentDocument.createElement("div");
+                    var i = contentDocument.createElement("img");
+
+                    i.setAttribute("alt", "");
+                    i.setAttribute("width", "59px");
+                    i.setAttribute("height", "59px");
+
+                    //console.log(a['magnifierGlass']);
+                    i.setAttribute("src", _private.imageUrl);
+                    d.appendChild(i);
+
+                    ColorPicker.colorPickerViewer.appendChild(d);
+
+                    $('body').append(ColorPicker.colorPickerViewer);
+                }
+                _public.showMagnifier = true;
+            });
+
+            _private.retrieveToolbar().then(function() {
+                if (!contentDocument.getElementById("colorPickerDiv")) {
+                    _private.colorPickerToolbar = contentDocument.createElement("Div");
+                    _private.colorPickerToolbar.setAttribute("id", "colorPickerDiv");
+
+                    _private.colorDiv = contentDocument.createElement("Div");
+                    _private.colorDiv.setAttribute("id", "colorDiv");
+                    _private.colorPickerToolbar.appendChild(_private.colorDiv);
+
+                    _private.colorTxt = contentDocument.createElement("Span");
+                    _private.colorTxt.setAttribute("id", "colorTxt");
+                    _private.colorPickerToolbar.appendChild(_private.colorTxt);
+
+                    $('body').append(_private.colorPickerToolbar);
+                };
+                _public.showToolbar = true;
+            });
+        },
+
+        destroy: function(contentDocument) {
+            $("#colorPickerCursor").remove();
+            $("colorPickerCss").remove();
+
+            $("#colorPickerDiv").remove();
+            $("#colorPickerViewer").remove();
+
+            contentDocument.removeEventListener("click", _private.Click, true);
+            contentDocument.removeEventListener("mousemove", _private.MouseMove, false);
+            _private.restoreTitles();
+        },
+
+    }
+
+    var _public = {
+        showMagnifier: false,
+        colorPickerViewer: null,
+        Show: function(contentDocument) {
+            _private.init(contentDocument);
+        },
+
+        Hide: function(contentDocument) {
+            try {
+                _private.destroy(contentDocument);
+            } catch (err) {
+                console.log(err);
+            };
+        },
+
+        refresh: function() {
+            chrome.extension.sendMessage({
+                type: "get-canvas"
+            });
+        },
+
+        setColor: function(color, type) {
+            _private.colorDiv.setAttribute("style", "position:fixed; width:18px; height:18px; background-color:" + color + ";");
+            _private.colorTxt.innerHTML = color;
+        },
+
+        setColors: function(colors, type) {
+
+            if (!ColorPicker.showMagnifier || !ColorPicker.colorPickerViewer) return;
+
+            var deep = colors.length;
+            m = (deep - 1) / 2;
+            var s = ''; //<table style="border-collapse:collapse;" cellspacing="1" >';
+            for (i = 0; i < deep; i++) {
+                s += '<tr>';
+
+                for (j = 0; j < deep; j++) {
+                    color = colors[j][i];
+                    if (!color) {
+                        color = 'indigo';
+                    }
+                    var centre = i == m && j == m;
+                    s += '<td style="background-color:' + color;
+                    if (centre) {
+                        s += '; border-radius:6px'
+                    }
+                    s += ';">';
+                    //if(centre) {
+                    //  s+='<div style="padding:0px; width:5px; height:5px; border:1px solid red; background-color:transparent;"></div>';
+                    //}
+                    s += '</td>';
+                }
+
+                s += '</tr>';
+            }
+            this.colorPickerViewer.childNodes[0].innerHTML = s;
+        },
+
+    }
+
+    return _public;
+
+}();
+>>>>>>> parent of 5d5221b... Central marker
---
+++ ColorContrast/background.js
@@ -0,0 +1,279 @@
+var Background = Background || {};
+
+// Converts an RGB color into a hex color
+Background.convertRGBToHex = function(rgb)
+{
+  var blue  = parseInt(rgb[2], 10).toString(16).toLowerCase();
+  var green = parseInt(rgb[1], 10).toString(16).toLowerCase();
+  var red   = parseInt(rgb[0], 10).toString(16).toLowerCase();
+
+  // If the color is only 1 character
+  if(blue.length == 1)
+  {
+    blue = "0" + blue;
+  }
+
+  // If the color is only 1 character
+  if(green.length == 1)
+  {
+    green = "0" + green;
+  }
+
+  // If the color is only 1 character
+  if(red.length == 1)
+  {
+    red = "0" + red;
+  }
+
+  return "#" + red + green + blue;
+};
+
+$.getContext = function() {
+
+  var deferred = $.Deferred();
+
+  chrome.tabs.captureVisibleTab(null, function(dataUrl)
+  {
+    Background.image = new Image();
+
+    Background.image.onload = function()
+    {
+      var canvas  = document.createElement("canvas");
+      Background.context = canvas.getContext("2d");
+
+      canvas.height = Background.image.naturalHeight;
+      canvas.width  = Background.image.naturalWidth;
+
+      Background.context.clearRect(0, 0, Background.image.naturalWidth, Background.image.naturalHeight);
+      Background.context.drawImage(Background.image, 0, 0);
+
+      deferred.resolve();
+    };
+
+    Background.image.onerror = function() {
+      deferred.reject();
+    };
+
+    Background.image.src = dataUrl;
+
+  });
+
+  return deferred.promise();
+}
+
+Background.context = null;
+Background.image = null;
+Background.promise = null; 
+Background.Color = null;
+Background.RequestColor = null;
+
+// Gets the current color
+Background.getColor = function(x, y, eventType)
+{
+  if(!Background.context || eventType=='selected')
+  {
+    Background.promise = $.getContext();
+  }
+
+  Background.promise.then(
+    function() {
+      var deep=3;
+      var color = Background.convertRGBToHex(Background.context.getImageData(x, y, 1, 1).data);
+      if(eventType=='selected') {
+        Background.Color = color;
+      }
+      
+      var colors = "";
+      var cr ='[';
+      for (i=-deep; i<=deep; i++) {
+        xi = x+i;
+        var cc = cr+"[";
+        for (j=-deep; j<=deep; j++) {
+          yj = y+j;
+          if(xi<0 || xi>=Background.image.naturalWidth || yj<0 || yj>=Background.image.naturalHeight)
+          {
+            colors+=cc + 'null';
+          }
+          else {
+            colors+=cc + "'"+Background.convertRGBToHex(Background.context.getImageData(xi, yj, 1, 1).data)+"'";
+          }
+          cc = ',';
+        }
+        colors+="]";
+        cr=',';
+      }
+      colors+="]";
+      //console.log(colors);
+      try {
+        chrome.tabs.executeScript(null, { "code": 
+          "if(ColorPicker) {\n"+
+          "  ColorPicker.setColors(" + colors + ", '" + eventType + "');\n" +
+          "  ColorPicker.setColor('" + color + "', '" + eventType + "');\n" + 
+          "}\n" });
+      }
+      catch(err) {
+        console.log(err);
+      }
+    }
+  );
+
+  return {};
+};
+
+// Returns the edit CSS dashboard HTML template
+Background.getEditCSSDashboardTemplates = function(parameters)
+{
+  return { "dashboard": ich.dashboard(parameters, true), "editCSS": ich.editCSSPanel(parameters, true), "panel": ich.dashboardPanel(parameters, true), "tab": ich.dashboardTab(parameters, true) };
+};
+
+// Returns the edit CSS tab HTML template
+Background.getEditCSSTabTemplates = function(parameters)
+{
+  return { "panel": ich.editCSSTabPanel(parameters, true), "tab": ich.editCSSTab(parameters, true) };
+};
+
+// Returns the element information dashboard HTML template
+Background.getElementInformationDashboardTemplates = function(parameters)
+{
+  return { "dashboard": ich.dashboard(parameters, true), "elementInformation": ich.elementInformationPanel(parameters, true), "panel": ich.dashboardPanel(parameters, true), "tab": ich.dashboardTab(parameters, true) };
+};
+
+// Gets the styles from CSS
+Background.getStylesFromCSS = function(cssDocuments)
+{
+  var contentDocument = null;
+  var cssContent      = null;
+  var styles          = "";
+  var documents       = cssDocuments.documents;
+  var styleSheets     = [];
+
+  // Loop through the documents
+  for(var i = 0, l = documents.length; i < l; i++)
+  {
+    contentDocument = documents[i];
+    styleSheets     = styleSheets.concat(contentDocument.styleSheets);
+
+    // If there are embedded styles
+    if(contentDocument.embedded)
+    {
+      styles += contentDocument.embedded;
+    }
+  }
+
+  cssContent = Background.getURLContents(styleSheets, "");
+
+  // Loop through the CSS content
+  for(i = 0, l = cssContent.length; i < l; i++)
+  {
+    styles += cssContent[i].content;
+  }
+
+  return { "css": styles };
+};
+
+// Gets the content from a URL
+Background.getURLContent = function(url, errorMessage)
+{
+  var content = null;
+
+  // Try to get the content
+  try
+  {
+    var request = new XMLHttpRequest();
+
+    // Chrome no longer allows a timeout set on synchronous requests
+    //request.timeout = Common.requestTimeout;
+
+    request.ontimeout = function()
+    {
+      content = errorMessage;
+    };
+
+    request.open("get", url, false);
+    request.send(null);
+
+    content = request.responseText;
+  }
+  catch(exception)
+  {
+    content = errorMessage;
+  }
+
+  return content;
+};
+
+// Gets the content from a set of URLs
+Background.getURLContents = function(urls, errorMessage)
+{
+  var url         = null;
+  var urlContents = [];
+
+  // Loop through the urls
+  for(var i = 0, l = urls.length; i < l; i++)
+  {
+    url = urls[i];
+
+    urlContents.push({ "content": Background.getURLContent(url, errorMessage), "url": url });
+  }
+
+  return urlContents;
+};
+
+// Initializes a generated tab
+Background.initializeGeneratedTab = function(url, data, locale)
+{
+  var extensionTab = null;
+  var tabs         = chrome.extension.getViews({ "type": "tab" });
+
+  // Loop through the tabs
+  for(var i = 0, l = tabs.length; i < l; i++)
+  {
+    extensionTab = tabs[i];
+
+    // If the tab has a matching URL and has not been initialized
+    if(extensionTab.location.href == url && !extensionTab.Generated.initialized)
+    {
+      extensionTab.Generated.initialized = true;
+
+      extensionTab.Generated.initialize(data, locale);
+    }
+  }
+};
+
+// Initializes a validation tab
+Background.initializeValidationTab = function(url, data)
+{
+  var extensionTab = null;
+  var tabs         = chrome.extension.getViews({ "type": "tab" });
+
+  // Loop through the tabs
+  for(var i = 0, l = tabs.length; i < l; i++)
+  {
+    extensionTab = tabs[i];
+
+    // If the tab has a matching URL and has not been initialized
+    if(extensionTab.location.href == url && !extensionTab.Validation.initialized)
+    {
+      extensionTab.Validation.initialized = true;
+
+      extensionTab.Validation.initialize(data);
+    }
+  }
+};
+
+// Handles any background messages
+Background.message = function(msg, sender, sendResponse)
+{
+  // If the msg type is to get the current color
+  if(msg.type == "get-color")
+  {
+    sendResponse(Background.getColor(msg.x, msg.y, msg.eventType));
+  }
+  else if(msg.type == "get-canvas")
+  {
+    Background.context = null;
+  }
+};
+
+chrome.extension.onMessage.addListener(Background.message);
+>>>>>>> parent of 5d5221b... Central marker
