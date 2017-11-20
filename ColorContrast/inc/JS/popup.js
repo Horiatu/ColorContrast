f51a0f010daf0013557ecda20554f33957cd328e
Horia Tudosie <htudosie@esri.ca>
2017-11-20 14:21:19 -0500

    Added Analytics

--- ColorContrast/inc/JS/popup.js
+++ ColorContrast/inc/JS/popup.js
@@ -1,4 +1,26 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-109917224-2']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+})();
+
+function trackButtonClick(e) {
+    _gaq.push(['_trackEvent', e.target.id, 'clicked']);
+}
+
 $(document).ready(function() {
+
+    var buttons = document.querySelectorAll('button');
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', trackButtonClick);
+    }
 
     getSelectedTab = function() {
         var dfd = $.Deferred();
